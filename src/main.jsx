c1fa103d4595e4fde171efc10153c7686d4d946b
kretz0815-svg <kretz0815@gmail.com>
2026-03-22 10:18:14 +0100

    feat: Mobile optimization for Project M with responsive CSS and touch drag-and-drop polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { polyfill } from "mobile-drag-drop"
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour"
+import "mobile-drag-drop/default.css"
 import './index.css'
 import App from './App.jsx'
 import { KLRGameProvider } from './features/klr'
 import { ProjectMProvider } from './features/project-m'
+
+// Enable drag and drop for touch devices globally
+polyfill({
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+});
+
+// Polyfill fix for iOS scrolling conflicts during drag
+window.addEventListener('touchmove', function() {}, { passive: false });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
